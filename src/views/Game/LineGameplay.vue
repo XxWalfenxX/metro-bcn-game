9f04ff0c991ea84be53ba5922e58ede450002458
Walfen <xxwalfenxx@gmail.com>
2023-11-14 02:05:10 +0100

    fix localization

--- src/views/Game/LineGameplay.vue
+++ src/views/Game/LineGameplay.vue
@@ -90,7 +90,7 @@ export default {
                 } else {
                     Swal.fire({
                         icon: "warning",
-                        title: "¡Se te ha agotado el tiempo!",
+                        title: "¡Se ha agotado el tiempo!",
                         customClass: {
                             popup: 'dark:bg-gray-800',
                             title: 'dark:text-white',
@@ -185,7 +185,7 @@ export default {
         <div class="mb-6">
             <input type="text" id="estacion_input" v-model="nombreEstacionInput2" :disabled="isInputDisabled"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Introduce el nombre" @keydown="enviar">
+                placeholder="Introduce el nombre de la estación" @keydown="enviar">
         </div>
         <button type="button" v-if="isInputDisabled" @click="$router.go(0)"
             class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Volver
